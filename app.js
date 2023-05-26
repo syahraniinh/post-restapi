867fa1861f5441253224e8350f431e5fbef59241
syahraniinh <syahraninh@mhs.mdp.ac.id>
2023-05-26 11:26:21 +0700

    DONEEEEEEEEEEEEEEEEEE

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv/config')
+
+app.use(bodyParser.urlencoded({
+  extended : true
+}));    
+
+app.use(bodyParser.json());;
+app.use(cors())
 
 app.get("/", (req, res) => {
-  res.send("Hello World 2!");
+  res.send("Hello World 2!")
 })
 
-app.listen(port, (process.env.PORT) => {
-  console.log(`Example app listening on port ${port}`);
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+let db = mongoose.connection
+
+//handle error
+db.on('error', console.error.bind(console, 'Error estabilishing a database connection'))
+
+//handle success
+db.once('open', () => {
+  console.log('Database is connected')
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
 })
